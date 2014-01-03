fb72f86af1a48737912caaebe2167c419cfb9926
KITAJIMA Akimasa <marumestring@gmail.com>
2014-01-03 13:13:11 +0900

    投稿用メッセージの作成できること確認

---
+++ abbrabot.rb
@@ -0,0 +1,334 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+include Math
+
+#require 'rubygems'
+#gem('twitter','0.9.8')
+require 'pp'
+require 'twitter'
+require 'logger'
+require 'fileutils'
+require 'date'
+require 'json'
+require 'net/http'
+require 'kconv'
+require 'open-uri'
+#require 'babelphish'
+#require 'easy_translate'
+require 'cgi'
+require 'erb'
+require 'rss'
+# ログファイル名
+LOG_FILE = File.expand_path "./abbrabot.log"
+def write_log msg
+  if $DEBUG
+    pp msg
+  else
+    log = Logger.new LOG_FILE, "weekly"
+    log.info msg
+    log.close
+  end
+end
+
+Already_Tweeted = "already_tweeted"
+def get_cont source,item_EXP,title_EXP,abst_EXP
+  rss = URI(source)
+  begin
+    urls = rss.read.toutf8.scan(item_EXP).flatten.map{ |x| rss.merge(x)}
+  rescue
+    write_log "get_cont rss read faild from #{source}: #{$!.class}: #{$!.message}"
+    return false
+  end
+  atFile = File.open(Already_Tweeted).read
+  urls.each do |x|
+    unless atFile =~ Regexp.union(x.to_s)
+      begin
+        str = x.read.toutf8
+      rescue
+        write_log "get_cont faild from #{source}: #{$!.class}: #{$!.message}"
+        return false
+      end
+      title = $1.strip if str =~ title_EXP
+      abst = $1.strip if str =~ abst_EXP
+      return [x.to_s,title,abst] if abst && title && (abst != "")
+    end
+  end
+  return false  
+end
+def get_from_arXiv source
+  item_EXP = %r!"list-identifier"><a href="(.*?)"!
+  abst_EXP = %r!Abstract:</span> (.*?)</blockquote>!m
+  title_EXP = %r!Title:</span>(.*?)</h1>!m
+  return get_cont source,item_EXP,title_EXP,abst_EXP
+end
+def get_from_APS source
+  item_EXP = %r!<rdf:li rdf:resource=\"(.*?)\"/>!
+  abst_EXP = %r!<div class='aps-abstractbox'><p>(.*?)</p>!m
+  title_EXP = %r!<h1>(.*?)</h1>!m
+  return get_cont source,item_EXP,title_EXP,abst_EXP
+end
+def get_from_ScienceDirect source
+  atFile = File.open(Already_Tweeted).read
+  begin
+    rss = RSS::Parser.parse(source)
+  rescue
+    write_log "cannot read RSS from #{source}: #{$!.class}: #{$!.message}"
+    return false
+  end
+  rss.items.each do |item|
+    next if (!item.link || atFile =~ Regexp.union(item.link) || !item.title)
+    cont = item.description.split('<br>')[3]
+    next if (!cont)
+    return [item.link,item.title,cont]
+  end
+  return false
+end
+#def get_from_ScienceDirect source
+#  item_EXP = %r!<link>(.*?)</link>!
+#  title_EXP = %r!<div class="articleTitle">(.*?)</div>!m
+#  abst_EXP = %r!<h3 class="h3">Abstract</h3><a name=".+?"></a><p>(.*?)</p>!m
+#  doi_EXP = %r!<a id="ddDoi" href="(.*?)"!
+#  rss = URI(source)
+#  begin
+#    urls = rss.read.toutf8.scan(item_EXP).flatten.map{ |x| CGI.unescapeHTML(x)}
+#  rescue
+#    write_log "to get content from #{source} is failed"
+#    return false
+#  end
+#  atFile = File.open(Already_Tweeted).read
+#  urls.each do |x|
+#    next unless x =~ %r!http://www.sciencedirect.com/science!
+#    begin
+#      str = URI(x).read.toutf8
+#    rescue
+#      write_log "to get content from #{x} is failed"
+#      next
+#    end
+#    doi = str[doi_EXP,1].strip
+#    next if atFile =~ Regexp.union(doi)
+#    title = $1.strip if str =~ title_EXP
+#    abst = $1.strip if str =~ abst_EXP
+#    if abst && title
+#      return [doi,title,abst] unless (abst == "" || abst =~ /Abstract not available/ || title == abst)
+#    end
+#  end
+#  return false
+#end
+def get_from_PNAS source
+  item_EXP = %r!<rdf:li rdf:resource=\"(.*?)\" />!
+  title_EXP = %r!<title><\!\[CDATA\[(.*?)\[!m
+  abst_EXP = %r!<description><\!\[CDATA\[(.*?)\]!m
+  rss = URI(source).read
+  urls = rss.toutf8.scan(item_EXP).flatten
+  begin
+    atFile = File.open(Already_Tweeted).read
+  rescue
+    write_log "PNAS READ ERROR: #{$!.class}: #{$!.message}"
+    return false
+  end
+  urls.each do |x|
+    unless atFile =~ Regexp.union(x)
+      cont_EXP = %r!<item rdf:about="#{Regexp.escape x}">(.*?)</item>!m
+      p cont_EXP if $DEBUG
+      str = $1 if rss =~ cont_EXP
+      if str
+        title = $1 if str =~ title_EXP
+        abst = $1 if str =~ abst_EXP
+        return [x,title,abst] if abst && title && (abst != "")
+      end
+    end
+  end
+  return false
+end
+def get_from_IOP source
+  atFile = File.open(Already_Tweeted).read
+  begin
+    rss = RSS::Parser.parse(source,false)
+  rescue
+    write_log "cannot read RSS from #{source}: #{$!.class}: #{$!.message}"
+    return false
+  end
+  rss.items.each do |item|
+    next if atFile =~ Regexp.union(item.link)
+    return [item.link,item.title,item.description.gsub("\r\n",' ')]
+  end
+  return false
+end
+#def get_from_IOP source
+#  item_EXP = %r!<rdf:li resource="(.*?)" />!
+#  title_EXP = %r!<title>(.*?)</title>!m
+#  abst_EXP = %r!<description>(.*?)</description>!m
+#  begin
+#    rss = URI(source).read
+#  rescue
+#    write_log "scanning rss: #{source} is failed"
+#    return false
+#  end
+#  urls = rss.toutf8.scan(item_EXP).flatten
+#  atFile = File.open(Already_Tweeted).read
+#  urls.each do |x|
+#    unless atFile =~ Regexp.union(x)
+#      cont_EXP = %r!<item rdf:about="#{Regexp.escape x}">(.*?)</item>!m
+#      p cont_EXP if $DEBUG
+#      str = $1 if rss =~ cont_EXP
+#      if str
+#        title = $1 if str =~ title_EXP
+#        abst = $1 if str =~ abst_EXP
+#        return [x,title,abst].map{ |y| CGI.unescapeHTML y} if abst && title && (abst != "")
+#      end
+#    end
+#  end
+#  return false
+#  # abst_EXP = %r!<dd id="articleAbsctract">.*?<P>(.*?)</P>!im
+#  # return get_cont source,item_EXP,title_EXP,abst_EXP
+#end
+def bit_ly url
+  url = ERB::Util.url_encode(url)
+  result = JSON.parse(Net::HTTP.get("api.bit.ly", "/shorten?#{query}"))
+  short_url = ""
+  result['results'].each_pair { |long_url, value| short_url = value['shortUrl'] }
+  return short_url
+end
+class String
+  def unescapeHTML
+    str = CGI.unescapeHTML(self)
+    str = str.gsub('&#8212',' ー ')
+    str = str.gsub(/&#\d+{1,4};/,' ')
+    return str
+  end
+  def delHTMLtag
+    return self.gsub %r!</?.*?>!,""
+  end
+  def abbr
+    ret = self.gsub(/([ァ-ン])([ァ-ンー]+)/){$1 + $2.gsub(/[ァ-オンャュョッ]/,"") }
+    ret.gsub!(/[ぁ-んー\n\r性的我々$]/,"")
+    ret.gsub!(/([。、（） ])、/,'\1')
+    ret.gsub!(/^[、。]/,"")
+    return ret
+  end
+  def to_ja
+    str = self.size > 1000 ? self[/\A(.{1000})/,1] : self
+    #return Babelphish::Translator.translate(str,'ja')
+    # Exite翻訳
+    puts 'input string is '+str if $DEBUG
+    if Net::HTTP.post_form(URI.parse('http://www.excite.co.jp/world/english/'),{ "before" => str}).body =~ %r!<textarea id="after".*?>(.*)</textarea>!m
+      puts 'output string is '+$1.toutf8 if $DEBUG
+      return $1.toutf8 if Net::HTTP.post_form(URI.parse('http://www.excite.co.jp/world/english/'),{ "before" => str}).body =~ %r!<textarea id="after".*?>(.*)</textarea>!m
+    end
+    return nil
+  end
+end
+def mkMessage content
+  p content if $DEBUG
+  content.map!{ |x| x.gsub(/[\n\r]/,' ')}
+  url = bit_ly content[0]
+  title = (content[1].unescapeHTML.delHTMLtag).unescapeHTML.to_ja.unescapeHTML.abbr
+  abst = (content[2].unescapeHTML.delHTMLtag).unescapeHTML.split("\n").map{ |x| x.strip}.join(' ').to_ja
+  #title = CGI.unescapeHTML(CGI.unescapeHTML(content[1].delHTMLtag).to_ja.abbr)
+  #abst = CGI.unescapeHTML(content[2].delHTMLtag).to_ja
+  if abst =~ /。/
+    $'.strip! 
+    abst = $' if $'.size>0
+  end
+  p abst.unescapeHTML if $DEBUG
+  ret = title + " " + url + " " + abst.unescapeHTML.abbr
+  p ret if $DEBUG
+  p ret[/(^.{140})/,1] if $DEBUG
+  return ret[/\A(.{140})/,1] if ret.chars.count > 140
+  return ret
+end
+
+if __FILE__ == $0
+  unless $DEBUG
+    Twitter.configure do |config|
+    end
+  end
+  #PID_FILE = File.expand_path "./#{File.basename(__FILE__,'.rb')}.pid"
+  # # コマンドラインオプション -k が指令されたら、プロセスをkillする
+  # if ARGV.shift == '-k'
+  #   if File.exist? PID_FILE
+  #     Process.kill :SIGTERM, File.read(PID_FILE).to_i
+  #     File.unlink PID_FILE
+  #   end
+  #   exit 0
+  # end
+  #File.open(PID_FILE,'w'){ |x| x.print Process.pid}
+  source_PRL = ['http://feeds.aps.org/rss/tocsec/PRL-SoftMatterBiologicalandInterdisciplinaryPhysics.xml']
+  source_PRE = ['http://feeds.aps.org/rss/tocsec/PRE-Biologicalphysics.xml',
+                'http://feeds.aps.org/rss/tocsec/PRE-Statisticalphysics.xml',
+                'http://feeds.aps.org/rss/tocsec/PRE-Interdisciplinaryphysics.xml',
+                'http://feeds.aps.org/rss/tocsec/PRE-Chaosandpatternformation.xml',
+                'http://feeds.aps.org/rss/tocsec/PRE-rapids.xml']
+  source_PhysicaD = ['http://rss.sciencedirect.com/publication/science/5537']
+  source_PhysicsLettersA = ['http://rss.sciencedirect.com/publication/science/5538']
+  source_EPL = ['http://iopscience.iop.org/0295-5075/?rss=1']
+  source_arXiv_qBio = ['http://www.arxiv.org/list/q-bio/pastweek?show=100']
+  source_arXiv_nlin = ['http://www.arxiv.org/list/nlin.CG/pastweek?show=100',
+                       'http://www.arxiv.org/list/nlin.AO/pastweek?show=100',
+                       'http://www.arxiv.org/list/nlin.CD/pastweek?show=100']
+  source_arXiv_condMat = ['http://www.arxiv.org/list/cond-mat.dis-nn/pastweek?show=100',
+                          'http://www.arxiv.org/list/cond-mat.other/pastweek?show=100',
+                          'http://www.arxiv.org/list/cond-mat.stat-mech/pastweek?show=100']
+  source_PNAS_Bio = ['http://www.pnas.org/rss/Biophysics_and_Computational_Biology.xml',
+                     'http://www.pnas.org/rss/Evolution.xml',
+                     #'http://www.pnas.org/rss/Systems_Biology.xml',
+                     'http://www.pnas.org/rss/Ecology.xml',
+                    'http://www.pnas.org/rss/Genetics.xml',
+                    'http://www.pnas.org/rss/Immunology.xml',
+                    'http://www.pnas.org/rss/Population_Biology.xml']
+  source_PNAS_Phys = ['http://www.pnas.org/rss/Applied_Physical_Sciences.xml',
+                      'http://www.pnas.org/rss/Computer_Sciences.xml',
+                      'http://www.pnas.org/rss/Physics.xml']
+  while true
+    now = Time.now
+    if (now.wday.between? 1,6) && (now.hour.between? 8,19)
+      sources = [source_PRL,source_PRE,
+                 source_EPL,
+                 source_PhysicaD,source_PhysicsLettersA,
+                 source_arXiv_qBio,source_arXiv_condMat,source_arXiv_nlin,
+                 source_PNAS_Bio,source_PNAS_Phys
+                ].shuffle #.map{ |x| x.shuffle}.shuffle.flatten
+      sources = [source_PNAS_Bio] if $DEBUG
+      cont = false
+      sources.each do |source|
+        source.shuffle.each do |x|
+          write_log x
+          if x =~ /feeds\.aps\.org/
+            cont = get_from_APS x
+          elsif x =~ /sciencedirect\.com/
+            cont = get_from_ScienceDirect x
+          elsif x =~ /iop\.org/
+            cont = get_from_IOP x
+          elsif x =~ /arxiv.org/
+            cont = get_from_arXiv x
+          elsif x=~ /pnas.org/
+            p x if $DEBUG
+            cont = get_from_PNAS x
+          end
+          break if cont
+        end
+        break if cont
+      end
+      if cont
+        write_log cont[0] unless $DEBUG
+        msg = mkMessage cont
+        if $DEBUG
+          p msg 
+        else
+          while msg
+            begin
+              Twitter.update msg unless $DEBUG
+            rescue
+              sleep $DEBUG ? 10 : 300 ;
+              next
+            end
+            write_log msg
+            File.open(Already_Tweeted,"a"){ |file| file.puts cont[0] }
+            msg = nil
+          end
+        end
+      end
+    end
+    sleep 1800
+  end
+end
